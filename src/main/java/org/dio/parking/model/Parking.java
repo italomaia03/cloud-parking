2802ade8eb7c269918b0c156de21b07f298ff845
italomaia03 <italo.fernandes.om@gmail.com>
2022-11-12 01:19:12 -0300

    Implement annotations for creating entity in DB

--- src/main/java/org/dio/parking/model/Parking.java
+++ src/main/java/org/dio/parking/model/Parking.java
@@ -1,9 +1,15 @@
 package org.dio.parking.model;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
+@Entity
 public class Parking {
 
+    @Id
     private String id;
     private String license;
     private String state;
