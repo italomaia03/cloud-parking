e27fb494368fc12f6585d9fdd2c819dc8ba5fa12
italomaia03 <italo.fernandes.om@gmail.com>
2022-11-12 01:12:15 -0300

    Implement persistency using PostgreSQL

---
+++ src/main/java/org/dio/parking/repository/ParkingRepository.java
@@ -0,0 +1,10 @@
+package org.dio.parking.repository;
+
+import org.dio.parking.model.Parking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParkingRepository extends JpaRepository<Parking, String> {
+
+}
